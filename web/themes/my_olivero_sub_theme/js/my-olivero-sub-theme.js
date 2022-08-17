799086b49e6b7f8ebe1d93292de7b3bcc647aa77
ESSADDOUQUI-ES-SLIMANI <mourad.essaddouqui-es-slimani@capgemini.com>
2022-08-17 21:07:25 +0100

    first commit

---
+++ web/themes/my_olivero_sub_theme/js/my-olivero-sub-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * my_olivero_sub_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.myOliveroSubTheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
